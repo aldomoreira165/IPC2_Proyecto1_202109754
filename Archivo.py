1e99d1ec568cfda9ac4ef5c83645c180adbf8361
aldomoreira165 <aldovasquez2014@gmail.com>
2022-08-23 00:44:45 -0600

    ingresando datos al constructor de paciente desde archivo xml

--- Archivo.py
+++ Archivo.py
@@ -1,3 +1,5 @@
+from platform import python_branch
+from turtle import pos
 import xml.etree.ElementTree as ET
 from Paciente import Paciente
 from Lista import Lista
@@ -10,24 +12,32 @@ class Archivo():
     lista_pacientes = Lista()
     lista_posiciones_celulas_infectadas = Lista()
 
+    #abriendo documento xml
     try:
         xml_file = open("./datos.xml", encoding="utf-8")
         if xml_file.readable():
             xml_data = ET.fromstring(xml_file.read())
-            for paciente in xml_data.findall('paciente'):
-                m = (paciente.find('m').text)
-                #lista_posiciones_celulas_infectadas.agregar_final(posicion)
-                #item_paciente = Paciente(paciente.find('datospersonales/nombre').text, paciente.find('datospersonales/edad').text, )
-                #lista_pacientes.agregar_final(item_paciente)
-            for celda in xml_data.findall('paciente/rejilla/celda'):
-                    x = celda.get('c')
-                    y =  celda.get('f')
+            contador = 0
             
-                
+            #obteniendo datos de paciente
+            for paciente in xml_data.findall('paciente'):
+                nombre = paciente.find('datospersonales/nombre').text
+                edad = int(paciente.find('datospersonales/edad'))
+                m = int(paciente.find('m').text)
+                periodos = int(paciente.find('periodos').text)
+
+                #obteniendo posicion de celdas contagiadas
+                for celda in xml_data[contador][3]:
+                    x = int(celda.get('c'))
+                    y = int(celda.get('f'))
+                    posicion = (m * y) + (x + 1)
+                    lista_posiciones_celulas_infectadas.agregar_final(posicion)
+                 
+                #agregando datos al constuctor del paciente        
+                item_paciente = Paciente(nombre, edad, m, periodos, lista_posiciones_celulas_infectadas)
+                lista_pacientes.agregar_final(item_paciente)
+                contador += 1
     except Exception as err:
         print("Error: ", err)
     finally:
         xml_file.close()
-    
-    #lista_pacientes.recorrer_inicio()
-   
--- Paciente.py
+++ Paciente.py
@@ -2,9 +2,9 @@ from Lista import Lista
 
 class Paciente:
     
-    def __init__(self, nombre, edad, m):
+    def __init__(self, nombre, edad, m,periodos, listaRejillasEnfermas):
         self.nombre = nombre
         self.edad = edad
         self.m = m
-        #self.periodos = periodos
-        self.listaRegillasEnfermas = Lista()
+        self.periodos = periodos
+        self.listaRejillasEnfermas = listaRejillasEnfermas
